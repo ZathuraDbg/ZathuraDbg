0dcb81de63ebab6d53e122320ac8ddeb77f9564a
HACKE-RC <60568652+HACKE-RC@users.noreply.github.com>
2024-05-20 20:58:46 +0530

    [Update] Added basic logging

--- src/app/dialogs/fileDialogs.cpp
+++ src/app/dialogs/fileDialogs.cpp
@@ -1,39 +1,56 @@
 #include "dialogHeader.hpp"
-
 std::string selectedFile = "/home/rc/Zathura-UI/src/test.asm";
 
-std::string openFileDialog(){
+bool fileDialogsSupported(){
     if (!pfd::settings::available())
     {
+        LOG_ERROR("PFD file dialogs are not supported on this platform");
         pfd::message("Unsupported platform!",
-                     "Sorry, file Dialogs are not available on this platform.\nPlease just copy paste the content of the file in the editor",
+                     "Sorry, File Dialogs are not available on this platform.\nPlease just copy paste the content of the file in the editor",
                      pfd::choice::ok,
                      pfd::icon::error);
+        return false;
+    }
+    return true;
+}
+
+std::string openFileDialog(){
+    if (!fileDialogsSupported()){
+        return "";
     }
 
     pfd::settings::verbose(false);
-
     auto f = pfd::open_file("Choose files to read", pfd::path::home(),
                             { "Text Files (.asm .s)", "*.asm *.s",
                               "All Files", "*" },
                             pfd::opt::none);
+
     if (!f.result().empty()){
         selectedFile = f.result()[0];
+        LOG_DEBUG("User selected the file " << selectedFile);
         return selectedFile;
     }
 
+    LOG_DEBUG("No file was selected");
     return "";
 }
 
 std::string saveAsFileDialog(){
+    if (!fileDialogsSupported()){
+        return "";
+    }
+
     auto f = pfd::save_file("Choose file to save",
                             pfd::path::home() + pfd::path::separator() + "readme.txt",
                             { "Text Files (.asm .s)", "*.asm *.s" },
                             pfd::opt::force_overwrite);
-    std::cout << "Selected file: " << f.result() << "\n";
 
     if (!f.result().empty()){
+        LOG_DEBUG("User selected the file " << f.result());
         return f.result();
     }
+
+
+    LOG_DEBUG("No file was selected" << f.result());
     return "";
 }
