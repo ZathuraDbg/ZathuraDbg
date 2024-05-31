52ba18babb3e9a7da56eaf688612d6d6fbf4f14f
HACKE-RC <60568652+HACKE-RC@users.noreply.github.com>
2024-05-31 22:34:21 +0530

    [Update] Even better error handling.

--- src/app/windows/stackWindow.cpp
+++ src/app/windows/stackWindow.cpp
@@ -16,24 +16,16 @@ void stackEditorWindow() {
     stackEditor.Cols = 8;
 
     ImGui::PushFont(io.Fonts->Fonts[3]);
+
     static char data[5 * 1024 * 1024];
-
-    // Read data from the stack
-    uc_mem_read(uc, STACK_ADDRESS, data, STACK_SIZE);
-
-    // Create a temporary buffer to hold the big-endian copy
     static char temp[5 * 1024 * 1024];
 
-    // Copy data to the temporary buffer in big-endian format
+    uc_mem_read(uc, STACK_ADDRESS, data, STACK_SIZE);
     copyBigEndian(temp, data, STACK_SIZE);
-
-    // Zero out the original data after copying
     memset(data, 0, STACK_SIZE);
-
-    // Copy the temporary buffer back to the original data buffer
     memcpy(data, temp, STACK_SIZE);
 
     // Draw the stack editor window
-    stackEditor.DrawWindow("Stack", (void*)((uintptr_t)data + 0x4), STACK_SIZE);
+    stackEditor.DrawWindow("Stack", (void*)((uintptr_t)data), STACK_SIZE);
     ImGui::PopFont();
 }
