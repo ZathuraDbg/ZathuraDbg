51874ece114644d263126ad12370000a01f7c1ad
HACKE-RC <60568652+HACKE-RC@users.noreply.github.com>
2024-06-13 11:22:12 +0530

    [Update] CMake to make life easier

---
+++ src/cmake-build-release/clean.cmake
@@ -0,0 +1,7 @@
+
+file(REMOVE
+    /home/rc/Zathura-UI/src/cmake-build-release/bin/Zathura
+/home/rc/Zathura-UI/src/cmake-build-release/CMakeFiles
+    /home/rc/Zathura-UI/src/cmake-build-release/cmake_install.cmake
+    /home/rc/Zathura-UI/src/cmake-build-release/Makefile
+)
