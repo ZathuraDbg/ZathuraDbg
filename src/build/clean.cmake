0a060573e2a06323947c2207134775d80a868536
HACKE-RC <60568652+HACKE-RC@users.noreply.github.com>
2024-06-13 13:31:17 +0530

    [Update] TextCallback added #20

---
+++ src/build/clean.cmake
@@ -0,0 +1,7 @@
+
+file(REMOVE
+    /tmp/Z/src/build/bin/Zathura
+/tmp/Z/src/build/CMakeFiles
+    /tmp/Z/src/build/cmake_install.cmake
+    /tmp/Z/src/build/Makefile
+)
---
+++ src/cmake-build-debug/clean.cmake
@@ -0,0 +1,7 @@
+
+file(REMOVE
+    /home/rc/Zathura-UI/src/cmake-build-debug/bin/Zathura
+/home/rc/Zathura-UI/src/cmake-build-debug/CMakeFiles
+    /home/rc/Zathura-UI/src/cmake-build-debug/cmake_install.cmake
+    /home/rc/Zathura-UI/src/cmake-build-debug/Makefile
+)
