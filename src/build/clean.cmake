4cc162e0c53f42e27d84c764a39f866daa7ebd92
HACKE-RC <60568652+HACKE-RC@users.noreply.github.com>
2024-06-28 21:30:11 +0530

    [Update] Removed make stuff

--- src/build/clean.cmake
+++
@@ -1,7 +0,0 @@
-
-file(REMOVE
-    /tmp/Zathura-UI/src/build/bin/Zathura
-/tmp/Zathura-UI/src/build/CMakeFiles
-    /tmp/Zathura-UI/src/build/cmake_install.cmake
-    /tmp/Zathura-UI/src/build/Makefile
-)
