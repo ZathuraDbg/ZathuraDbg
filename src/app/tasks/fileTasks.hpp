8330b753d7d840df4b68011828d32e3eb7c36328
HACKE-RC <60568652+HACKE-RC@users.noreply.github.com>
2024-07-05 17:53:10 +0530

    [Update] Json context loading and saving implemented

--- src/app/tasks/fileTasks.hpp
+++ src/app/tasks/fileTasks.hpp
@@ -10,6 +10,6 @@ void fileOpenTask(const std::string& fileName);
 void fileSaveAsTask(const std::string &fileName);
 void fileSaveTask(const std::string &fileName);
 extern void fileRunTask(uint64_t instructionCount = 0);
-extern void fileSaveUCContextAsJson(const std::string& fileName);
-extern void fileLoadUCContextFromJson(const std::string& fileName);
+extern void fileSaveUCContextAsJson(const std::string& jsonFilename);
+extern void fileLoadUCContextFromJson(const std::string& jsonFilename);
 #endif // !fileTasks
--- src/test.asm
+++ src/test.asm
@@ -1,2 +1,20 @@
-mov rax, 0x100
-mov rbx, rax
+main:
+    mov rax, 0x100
+    mov rbx, 0x200
+    add rbx, rax
+    mov rdi, rbx
+    call subtract_hundred 
+    cmp rax, 0x100
+    jne zero_regs
+
+subtract_hundred:
+    sub rdi, 0x100
+    mov rax, rdi
+    ret
+
+zero_regs:
+    mov rax, 0x00
+    mov rbx, rax 
+    mov rcx, rbx
+    mov rdx, rcx
+    hlt 
