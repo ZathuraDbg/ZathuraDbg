696951a6077a3b25fcdd1806231263a03c9be31b
HACKE-RC <60568652+HACKE-RC@users.noreply.github.com>
2024-08-31 13:50:01 +0530

    [Update] Breakpoint command working with line numbers

--- src/app/actions/actions.hpp
+++ src/app/actions/actions.hpp
@@ -4,5 +4,6 @@
 extern void runActions();
 extern void startDebugging();
 extern void debugContinueAction(bool skipBP = false);
+extern bool debugAddBreakpoint(int lineNum);
 //#include "../app.hpp"
 #endif //ZATHURA_ACTIONS_HPP
