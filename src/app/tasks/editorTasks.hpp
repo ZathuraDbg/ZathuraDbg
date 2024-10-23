bf159b8a175343d870d475db8b4555cf17b7ece8
HACKE-RC <60568652+HACKE-RC@users.noreply.github.com>
2024-10-23 14:00:24 +0530

    [Update] Detects and offers to disassemble shellcode when pasted in the input box. Implemented #83

--- src/app/tasks/editorTasks.hpp
+++ src/app/tasks/editorTasks.hpp
@@ -7,6 +7,7 @@
 extern TextEditor* editor;
 extern bool writeEditorToFile(const std::string& filePath);
 extern bool readFileIntoEditor(const std::string& filePath);
+extern void pasteCallback(std::string clipboardText);
 extern int labelCompletionCallback(ImGuiInputTextCallbackData* data);
 extern void createLabelLineMapCallback(std::map<std::string, int>& labelVector);
 extern std::pair<int, int> parseStrIntoCoordinates(std::string& popupInput);
